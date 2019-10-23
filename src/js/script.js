831678742fc95f1b8192f8133b56b0129f16449a
Jakub Kiełbasa <jakub.kiel@gmail.com>
2019-10-23 18:07:24 +0200

    Add get elements code in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -53,6 +53,7 @@
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.initAccordion();
      
       thisProduct.renderInMenu();     
       console.log('new Product:', thisProduct);
@@ -68,6 +69,15 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);    
     }
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }  
     initAccordion(){
       const  thisProduct = this;
         
